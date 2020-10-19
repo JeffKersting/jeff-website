c6bc203f10f652d47e88ebbdfed15ac909403508
Jeff Kersting <jeff.w.kersting@gmail.com>
2020-10-18 18:14:59 -0600

    Added grid functionality and button

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+
+                    //CLICK THE BUTTON FUNCTIONALITY//
+
+
+//Variable Declarations
 var buttonClick = document.querySelector(".clicks");
 var clickCounts = 0;
 var clicksDisplay = document.querySelector(".clickDisplay");
 var clickReact = document.querySelector("#clickReact");
-
-buttonClick.addEventListener("click", clickUp)
-
-
-
-
-//Click count button
+buttonClick.addEventListener("click", clickUp);
 clicksDisplay.innerText = clickCounts;
+
+//Button display functions
 function reactClear(){
   clickReact.innerText = "";
 }
@@ -39,11 +40,12 @@ function clickUp(){
   }
 }
 
-//Grid functionality
-
+                    //GRID FUNCTIONALITY//
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.addEventListener("keydown", function(e){
+
+//Event Listeners
+document.addEventListener("keydown", function(e){//arrow key event
     switch (e.keyCode){
       case 37:
       drawLeft();
@@ -59,55 +61,192 @@ document.addEventListener("DOMContentLoaded", () => {
       break;
     }
   });
-  const grid = document.querySelector(".gridDisplay");
-  var squares = Array.from(document.querySelectorAll(".innerGrid"));
-  var currentSquare = 0;
-  var rightLimits = [9,19,29,39,49,59,69,79,89,99];
-  var leftLimits = [0,10,20,30,40,50,60,70,80,90];
-  squares[currentSquare].classList.add("player")
+document.addEventListener("click", restartLevel);
 
-  function draw(){
-    squares[currentSquare].classList.add("player")
-  };
+//Variable declarations
+const grid = document.querySelector(".gridDisplay");
+var squares = Array.from(document.querySelectorAll(".innerGrid"));
+var restartButton = document.querySelector(".restartLevel");
+var playerSquare;
+var pushBlockSquare;
+var endBlockSquare;
+var obstacleBlockSquare;
+var currentLevel = 0;
+var rightLimits = [9,19,29,39,49,59,69,79,89,99];
+var leftLimits = [0,10,20,30,40,50,60,70,80,90];
+var upperLimits = [0,1,2,3,4,5,6,7,8,9];
+var lowerLimits = [90,91,92,93,94,95,96,97,98,99];
+var levels = [
+  {level: 1,
+  playerStart: 0,
+  pushBlockStart: 43,
+  endBlockStart: 99,
+  obstacleBlockStart: [44,45,54,55]},
 
-  function drawLeft(){
-    if(leftLimits.includes(currentSquare)) {
+  {level: 2,
+  playerStart: 1,
+  pushBlockStart: 21,
+  endBlockStart: 13,
+  obstacleBlockStart: [0,10,20,30,40,50,2,12,22,32,42,52]},
+
+  {level: 3,
+  playerStart: 3,
+  pushBlockStart: 12,
+  endBlockStart: 9,
+  obstacleBlockStart: [5,15,25,35, 50,51,52,53,54,55,56]},
+
+  {level: 4,
+  playerStart: 1,
+  pushBlockStart: 11,
+  endBlockStart: 9,
+  obstacleBlockStart: [2,12,22,32,42,60]},
+]
+
+//Grid rendering functions
+function drawLeft(){
+    if(leftLimits.includes(playerSquare)) {
       return;
-    }else{
-    squares[currentSquare].classList.remove("player")
-    currentSquare -= 1;
-    squares[currentSquare].classList.add("player")
+    }else if(leftLimits.includes(pushBlockSquare) && playerSquare === pushBlockSquare + 1){
+      return;
+    }else if(obstacleBlockSquare.includes(pushBlockSquare - 1) && pushBlockSquare + 1 === playerSquare){
+      return;
+    }else if(obstacleBlockSquare.includes(playerSquare - 1)){
+      return;
+    }else if (playerSquare === pushBlockSquare + 1){
+      squares[pushBlockSquare].classList.remove("pushBlock")
+      pushBlockSquare -= 1;
+      squares[pushBlockSquare].classList.add("pushBlock")
+      squares[playerSquare].classList.remove("player")
+      playerSquare -= 1;
+      squares[playerSquare].classList.add("player")
+      if(pushBlockSquare === endBlockSquare){
+        youWin();
+      }
+      return;
+    } else {
+      squares[playerSquare].classList.remove("player")
+      playerSquare -= 1;
+      squares[playerSquare].classList.add("player")
     }
   };
-  function drawUp(){
-    if(currentSquare <= 9){
+function drawUp(){
+    if(playerSquare <= 9){
       return;
-    }else{
-    squares[currentSquare].classList.remove("player")
-    currentSquare -= 10;
-    squares[currentSquare].classList.add("player")
-  }
-};
-  function drawRight(){
-    if(rightLimits.includes(currentSquare)) {
+    }else if(upperLimits.includes(pushBlockSquare) && playerSquare === pushBlockSquare + 10){
       return;
-    }else{
-    squares[currentSquare].classList.remove("player")
-    currentSquare += 1;
-    squares[currentSquare].classList.add("player")
+    }else if(obstacleBlockSquare.includes(pushBlockSquare - 10) && pushBlockSquare + 10 === playerSquare){
+      return;
+    }else if(obstacleBlockSquare.includes(playerSquare - 10)){
+      return;
+    }else if (playerSquare === pushBlockSquare + 10){
+      squares[pushBlockSquare].classList.remove("pushBlock")
+      pushBlockSquare -= 10;
+      squares[pushBlockSquare].classList.add("pushBlock")
+      squares[playerSquare].classList.remove("player")
+      playerSquare -= 10;
+      squares[playerSquare].classList.add("player")
+      if(pushBlockSquare === endBlockSquare){
+        youWin();
+      }
+      return;
+    } else {
+      squares[playerSquare].classList.remove("player")
+      playerSquare -= 10;
+      squares[playerSquare].classList.add("player")
     }
   };
-  function drawDown(){
-    if(currentSquare >= 90){
+function drawRight(){
+    if(rightLimits.includes(playerSquare)) {
+      return;
+    }else if(rightLimits.includes(pushBlockSquare) && playerSquare === pushBlockSquare - 1){
+      return;
+    }else if(obstacleBlockSquare.includes(pushBlockSquare + 1) && pushBlockSquare - 1 === playerSquare){
+      return;
+    }else if(obstacleBlockSquare.includes(playerSquare + 1)){
+      return;
+    }else if (playerSquare === pushBlockSquare - 1){
+      squares[pushBlockSquare].classList.remove("pushBlock")
+      pushBlockSquare += 1;
+      squares[pushBlockSquare].classList.add("pushBlock")
+      squares[playerSquare].classList.remove("player")
+      playerSquare += 1;
+      squares[playerSquare].classList.add("player")
+      if(pushBlockSquare === endBlockSquare){
+        youWin();
+      }
+      return;
+    } else {
+      squares[playerSquare].classList.remove("player")
+      playerSquare += 1;
+      squares[playerSquare].classList.add("player")
+    }
+  };
+function drawDown(){
+    if(playerSquare >= 90){
+      return;
+    }else if(lowerLimits.includes(pushBlockSquare) && playerSquare === pushBlockSquare - 10){
+      return;
+    }else if(obstacleBlockSquare.includes(pushBlockSquare + 10) && pushBlockSquare - 10 === playerSquare){
+      return;
+    }else if(obstacleBlockSquare.includes(playerSquare + 10)){
+      return;
+    }else if (playerSquare === pushBlockSquare - 10){
+      squares[pushBlockSquare].classList.remove("pushBlock")
+      pushBlockSquare += 10;
+      squares[pushBlockSquare].classList.add("pushBlock")
+      squares[playerSquare].classList.remove("player")
+      playerSquare += 10;
+      squares[playerSquare].classList.add("player")
+      if(pushBlockSquare === endBlockSquare){
+        youWin();
+      }
       return;
     }else{
-    squares[currentSquare].classList.remove("player")
-    currentSquare += 10;
-    squares[currentSquare].classList.add("player")
+      // squares[playerSquare].classList.add("playerDown"); Player movement draw example
+      // setTimeout(drawDownTransition, 70); left in incase wanting to reimplement
+      squares[playerSquare].classList.remove("player")
+      playerSquare += 10;
+      squares[playerSquare].classList.add("player")
     }
   };
 
-  function undraw(){
-    squares[currentSquare].classList.remove("player")
+function undrawLevel(){
+    squares[playerSquare].classList.remove("player");
+    squares[pushBlockSquare].classList.remove("pushBlock");
+    squares[endBlockSquare].classList.remove("endBlock");
+    obstacleBlockSquare.forEach(block => squares[block].classList.remove("obstacleBlock"));
   };
+function drawLevel(){
+    squares[playerSquare].classList.add("player");
+    squares[pushBlockSquare].classList.add("pushBlock");
+    squares[endBlockSquare].classList.add("endBlock");
+    obstacleBlockSquare.forEach(block => squares[block].classList.add("obstacleBlock")); //iterates thru obstacle block array and assigns class
+    currentLevel++;
+  };
+function drawLevelStart(){
+    playerSquare = levels[currentLevel].playerStart;
+    pushBlockSquare = levels[currentLevel].pushBlockStart;
+    endBlockSquare = levels[currentLevel].endBlockStart;
+    obstacleBlockSquare = levels[currentLevel].obstacleBlockStart;
+    drawLevel()
+  };
+function restartLevel(){
+    currentLevel--;
+    undrawLevel()
+    playerSquare = levels[currentLevel].playerStart;
+    pushBlockSquare = levels[currentLevel].pushBlockStart;
+    endBlockSquare = levels[currentLevel].endBlockStart;
+    obstacleBlockSquare = levels[currentLevel].obstacleBlockStart;
+    drawLevel()
+  };
+function youWin(){
+    undrawLevel()
+    playerSquare = levels[currentLevel].playerStart;
+    pushBlockSquare = levels[currentLevel].pushBlockStart;
+    endBlockSquare = levels[currentLevel].endBlockStart;
+    obstacleBlockSquare = levels[currentLevel].obstacleBlockStart;
+    drawLevel()
+  };
+
+document.querySelector("body").onload = drawLevelStart();
 });
